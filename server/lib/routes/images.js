2c99c943371136d7eb5326eb370b7e5dc3de64a6
CharlyWelch <blwbiology@gmail.com>
2018-06-18 14:35:29 -0700

    add image routes

---
+++ server/lib/routes/images.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const Image = require('../models/Image');
+const { getParam, respond } = require('./route-helpers');
+
+module.exports = router
+    .param('id', getParam)
+
+    .post('/', respond(
+        ({ body }) => Image.create(body)
+    ))
+
+    .get('/', respond(
+        ({ query }) => Image.findMyQuery(query)
+    ));
